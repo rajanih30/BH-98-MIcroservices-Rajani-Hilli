d268b430bd31d0282724d3476eb61d4c0922fbfa
Rajani Hilli <rajani_hilli@thbs.india.com>
2022-04-28 12:55:54 +0530

    28-4

--- demo-sboot/src/main/java/com/example/demo/DemoSbootApplication.java
+++ demo-sboot/src/main/java/com/example/demo/DemoSbootApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("com.example.demo")
 @SpringBootApplication
+@EnableJpaRepositories("com.example.demo")
 public class DemoSbootApplication {
 
 	public static void main(String[] args) {
---
+++ demo-sboot/src/main/java/com/example/demo/User.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MYUSER")
+public class User {
+ 
+	private @Id @GeneratedValue long id;
+	private String name;
+	
+	public User() {}
+	
+	public User(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+}
